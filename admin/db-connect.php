289c7e3a4c8f2d3da5433a73afd85c7eb4a5371f
WilliamsChazal <w.chazal@codeur.online>
2021-11-16 17:03:01 +0100

    last modif

--- admin/db-connect.php
+++ admin/db-connect.php
@@ -11,27 +11,6 @@ try {
     echo 'Error Connection : '.$e->getMessage();
 }
 
-/* $servername ='promo-68.codeur.online';
-$dbname ='williamsc_portfolio';
-$username ='williamsc';
-$password ='on3zxq186gb+nw==';
-try {
-    $db = new PDO("mysql:host=$servername; dbname=$dbname",$username, $password);
-    $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo 'Error Connection : '.$e->getMessage();
-} */
 
 
-/* <?php
-$servername = 'localhost';
-$dbname = 'backoffice';
-$username = 'root';
-$password = '';
-try {
-$db = new PDO("mysql:host=$servername; dbname=$dbname",$username,$password);
-$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo 'succes';
-} catch (PDOException $e) {
-echo 'connexion fail : '.$e->getMessage();
-} */
+

8522262b86ed9e53aa9ff8abebe736e6d3fdaff5
WilliamsChazal <w.chazal@codeur.online>
2021-11-16 16:44:43 +0100

    last modif

--- includes/header.php
+++ includes/header.php
@@ -23,6 +23,9 @@
         <script src="https://unpkg.com/uuid@latest/dist/umd/uuidv4.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="http://fonts.cdnfonts.com/css/digital-7-mono" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap" rel="stylesheet">
                 
 
         <title>Williams Chazal</title>
